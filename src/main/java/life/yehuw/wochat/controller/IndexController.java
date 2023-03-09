f6b98298720e23987d3caf5b74831d40f99bffec
Yehuw <wangloulou2@gmail.com>
2023-03-09 21:51:14 +0800

    add index and bootstrap

---
+++ src/main/java/life/yehuw/wochat/controller/IndexController.java
@@ -0,0 +1,14 @@
+package life.yehuw.wochat.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+}
