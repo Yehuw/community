79ab21ab8dbe2bf27f6386cd84d927290f3a9973
Yehuw <wangloulou2@gmail.com>
2023-03-10 10:52:14 +0800

    .properties

--- src/main/java/life/yehuw/wochat/controller/AuthorizeController.java
+++ src/main/java/life/yehuw/wochat/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.yehuw.wochat.dto.AccessTokenDTO;
 import life.yehuw.wochat.dto.GithubUser;
 import life.yehuw.wochat.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,14 +18,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("7570a99e98be1e5446c4");
-        accessTokenDTO.setClient_secret("2970c4fe6e101c70d6dbf6e859bf9f8f876b0d0c");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
