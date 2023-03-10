bead22c2496ba9c695dd01b0139a5a6d3a4c4f1b
Yehuw <wangloulou2@gmail.com>
2023-03-10 23:53:56 +0800

    h2 mybatis

--- src/main/java/life/yehuw/wochat/WochatApplication.java
+++ src/main/java/life/yehuw/wochat/WochatApplication.java
@@ -1,8 +1,11 @@
 package life.yehuw.wochat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//@MapperScan(basePackages = "life.yehuw.wochat.mapper")
 @SpringBootApplication
 public class WochatApplication {
 
--- src/main/java/life/yehuw/wochat/controller/AuthorizeController.java
+++ src/main/java/life/yehuw/wochat/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package life.yehuw.wochat.controller;
 
 import life.yehuw.wochat.dto.AccessTokenDTO;
 import life.yehuw.wochat.dto.GithubUser;
+import life.yehuw.wochat.mapper.UserMapper;
+import life.yehuw.wochat.model.User;
 import life.yehuw.wochat.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  *
@@ -17,6 +22,9 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -29,7 +37,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -37,8 +46,23 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+//        System.out.println(user.getName());
+        if(githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //登陆失败，重新登录
+            return "redirect:/";
+        }
+//        return "index";
     }
 }
